1423ce7d3cd55e46978881e0f864816f10d37578
Artem <adpm@list.ru>
2023-06-22 00:12:01 +0600

    add navigation between detail and list

---
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -0,0 +1,36 @@
+package com.example.dogs.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.dogs.R
+import com.example.dogs.databinding.FragmentListBinding
+
+
+class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonDetails.setOnClickListener {
+            findNavController().navigate(R.id.actionDetailFragment)
+        }
+    }
+
+
+}
